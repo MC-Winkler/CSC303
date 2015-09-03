a09b02f5791dac42a70c90e4f13873f2770d6bd8
unknown <mwinkler3@duke-27.fs.elon.edu>
2015-09-02 20:15:15 -0400

    Create ReceiveMessageActivity and make it display the text typed into the EditText in CreateMessageActivity

--- Messanger/app/src/main/java/com/hfad/messanger/CreateMessageActivity.java
+++ Messanger/app/src/main/java/com/hfad/messanger/CreateMessageActivity.java
@@ -2,19 +2,26 @@ package com.hfad.messanger;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
 import android.view.View;
+import android.content.Intent;
+import android.widget.EditText;
 
 public class CreateMessageActivity extends Activity {
 
     @Override
     protected void onCreate (Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_message)
+        setContentView(R.layout.activity_create_message);
     }
 
     //Call onSendMessage() when the button is clicked
     public void onSendMessage(View view){
-
+        EditText messageView = (EditText) findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
+        Intent intent = new Intent(this, RecieveMessageActivity.class);
+        intent.putExtra(RecieveMessageActivity.EXTRA_MESSAGE, messageText);
+        startActivity(intent);
     }
 }
 
